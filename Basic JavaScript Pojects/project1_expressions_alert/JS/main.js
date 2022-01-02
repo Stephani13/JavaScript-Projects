bcc4b330923cce4a5670f028ffbd25d0c896b37d
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-01-02 01:08:44 -0500

    basic javascript

---
+++ Basic JavaScript Pojects/project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+var A = "This is a string: \"Hello, World\"" + " String Concatenate challenge"; //creates a variable and add a string concatenation
+var A = A.fontcolor("red") //assigns the variable A a color
+document.write(A + " My first string concatenate") //prints the result on screen
+
+
+window.alert("Hello World");  //makes an alert box to pop up
+
+
+var animals = "domestic" , cat="Felix" , dog= "Filo" , bird="Yue";
+var animals= animals.fontcolor('blue') ;
+var dog = dog.fontcolor("pink");
+var bird = bird.fontcolor('purple');
+var cat= cat.fontcolor('brown')
+document.write(cat);
+
+
+document.write(3+4);
