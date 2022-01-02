bf412165bd9de4b7ff8f2adb43e00f038214d6a1
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-01-02 10:45:34 -0500

    new

--- Basic JavaScript Pojects/project2_functions/JS/main.js
+++ Basic JavaScript Pojects/project2_functions/JS/main.js
@@ -1,11 +1,11 @@
 function my_First_Function() {
-    var str="This text is in red";
-    var result= str.fontcolor("red");
+    var str="This text is in red"; //variable that assigns a value to str
+    var result= str.fontcolor("red"); //assigns a color to str
     document.getElementById("Red_Text").innerHTML = result;
 }
 
 function my_Function(){
-    var sentece="this sentence is";
-    sentece += " concatenated.";
+    var sentece="this sentence is"; //this creates a variable
+    sentece += " concatenated."; //operator that concatenates a string
     document.getElementById("concatenate").innerHTML = sentece;
 }
