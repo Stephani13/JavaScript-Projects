d4e49f4e0f193a2c3eca6ec4dc72a1459ee01381
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-01-02 23:28:02 -0500

    comments

--- Basic JavaScript Pojects/project4_Diccionaries/JS/main.js
+++ Basic JavaScript Pojects/project4_Diccionaries/JS/main.js
@@ -1,9 +1,9 @@
 function dictionary_Function(){
-    var Flowers = {
-        Species : "Lilies",
+    var Flowers = { //creates a dictionary
+        Species : "Lilies",//key-value (ensure values are in quotations to prevent errors)
         Solor : "Rose",
         Zone: "perennials",
     };
-    delete Flowers.Species;
+    delete Flowers.Species;//delete a dictionary element
     document.getElementById("Dictionary").innerHTML = Flowers.Species;
 }
